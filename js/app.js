ff0da17d835cef843c963c61de8bc35ce80f76c2
Yanyshpolska <acya2002@gmail.com>
2022-08-27 21:18:30 +0300

    third try, but I'll correct all this mess

--- js/app.js
+++ js/app.js
@@ -1,139 +1,131 @@
 "use strict";
+const playerWidth = 80;
+
 const CELL = {
   width: 101,
   height: 80,
+};
+const gameField = {
+  width: CELL.width * 5,
+  height: CELL.height * 5,
 };
 const playerStartPosition = {
   width: CELL.width * 2,
   height: CELL.height * 5,
 };
-
 const bugRacePosition = [62, 146, 230];
-// const bugSpeed =
+const getRandomNumber = (a) => Math.floor(Math.random() * a);
 
+// ===========SCORE block треба спробувати його опустити вниз=============
 const score = document.createElement("div");
-score.innerHTML = "SCORE";
-document.body.append(score);
-// Enemies our player must avoid
-// const Enemy = function (y, speed) {
-//   //     Setting the Enemy initial location (you need to implement)
-//   //     Setting the Enemy speed (you need to implement)
-//   this.speed = speed;
-//   this.x = 0;
-//   this.y = bugRacePosition[Math.floor(Math.random() * bugRacePosition.length)];
-//   this.sprite = "images/enemy-bug.png";
-// };
-
-//     Updates the Enemy location (you need to implement)
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-// Enemy.prototype.update = function (dt) {
-//   this.x = this.x + this.speed * dt;
-//   // You should multiply any movement by the dt parameter
-//   // which will ensure the game runs at the same speed for
-//   // all computers.
-// };
-// //     Handles collision with the Player (you need to implement)
-
-// Enemy.prototype.render = function () {
-//   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
-
+let scorePoints = 0;
+score.innerHTML = "SCORE: " + scorePoints;
+document.body.after(score);
+// ===========Об'єкт жука==================================================
 const Enemy = {
-  speed: Math.floor(Math.random() * 50 + 100),
-  x: -100,
-  y: bugRacePosition[Math.floor(Math.random() * bugRacePosition.length)],
+  x: -CELL.width,
   sprite: "images/enemy-bug.png",
+
   update: function (dt) {
     this.x = this.x + this.speed * dt;
-    if (this.x > 505) {
-      this.x = -100;
+    //  запускає жука заново в рандомний ряд
+    if (this.x > gameField.width) {
+      this.x = -CELL.width;
+      this.y = bugRacePosition[getRandomNumber(bugRacePosition.length)];
+      this.speed = getRandomNumber(70) + 100;
+      this.rowPosition = bugRacePosition.indexOf(this.y) + 1;
     }
+    this.kissPlayer();
   },
   render: function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   },
-  kissPlayer: function (player) {
-    if (this.x === player.x) {
+  kissPlayer: function () {
+    const startColl = player.x - playerWidth;
+    const stopColl = player.x + playerWidth;
+
+    if (
+      this.rowPosition === player.rowPosition &&
+      this.x >= startColl &&
+      this.x <= stopColl
+    ) {
       player.x = playerStartPosition.width;
       player.y = playerStartPosition.height;
+      player.rowPosition = 5;
     }
-    console.log(player.x);
   },
 };
 
+// ===========Об'єкт Player==================================================
 const Player = {
   x: playerStartPosition.width,
   y: playerStartPosition.height,
   score: 0,
-  sprite: "images/char-boy.png",
+  rowPosition: 5,
+  sprite: "images/char-cat-girl.png",
+
   render: function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   },
-  //     The update method for the Player (can be similar to the one for the Enemy)
+
   update: function (dt) {},
-  //     If the player reaches the water the game should be reset by moving the player back to the initial location (you can write a separate reset Player method to handle that).
+
   handleInput: function (pressedKey) {
     switch (pressedKey) {
       case "right":
-        this.x += 101;
+        this.x += CELL.width;
         break;
       case "left":
-        this.x += -101;
+        this.x += -CELL.width;
         break;
       case "up":
         this.y += -82;
+        this.rowPosition > 1 ? (this.rowPosition -= 1) : this.rowPosition;
         break;
       case "down":
         this.y += 82;
+        this.rowPosition < 5 ? (this.rowPosition += 1) : this.rowPosition;
         break;
     }
+    //  перевірка чи не вийшов за межі поля
     if (this.x > 404) {
       this.x = 404;
     } else if (this.x < 0) {
       this.x = 0;
     }
+    //   дійшов до води
     if (this.y > 400) {
       this.y = 400;
-    } else if (this.y < 15) {
+    } else if (this.y < 60) {
+      scorePoints += 1;
+      score.innerHTML = "SCORE: " + scorePoints;
       this.x = playerStartPosition.width;
       this.y = playerStartPosition.height;
+      this.rowPosition = 5;
     }
   },
 };
-
+// ===============створюємо жуків
 const allEnemies = [];
 
 function createNewBug() {
   const newEnemy = {
     __proto__: Enemy,
-    y: bugRacePosition[Math.floor(Math.random() * bugRacePosition.length)],
-    speed: Math.floor(Math.random() * 70 + 100),
+    x: getRandomNumber(gameField.width),
+    y: bugRacePosition[getRandomNumber(bugRacePosition.length)],
+    speed: getRandomNumber(70) + 100,
   };
+  newEnemy.rowPosition = bugRacePosition.indexOf(newEnemy.y) + 1;
   allEnemies.push(newEnemy);
 }
 // Place all enemy objects in an array called allEnemies
-setTimeout(createNewBug(), 500);
-setTimeout(createNewBug(), 5500);
-setTimeout(createNewBug(), 2500);
-setTimeout(createNewBug(), 2500);
-setTimeout(createNewBug(), 2500);
-
-// const enem = {};
-// enem.__proto__ = Enemy;
-// enem.speed = 0;
-// enem.x = -100;
-// allEnemies.push(enem);
-// let allEnemies = [new Enemy(62, 100), new Enemy(146, 150)];
-// // Place the player object in a variable called player
+createNewBug();
+// createNewBug();
+// createNewBug();
+// createNewBug();
+// createNewBug();
+// ===================створюємо гравця
 const player = { __proto__: Player };
-// class extension is implemented using Subclass.prototype = Object.create(Superclass.prototype),
-// not Subclass.prototype = new Superclass(params);
-// player.prototype = Object.create(Player.prototype);
-// player.__proto__ = Player;
-// console.log(allEnemies);
-// console.log(player);
-allEnemies.forEach((enem) => enem.kissPlayer(player));
 
 document.addEventListener("keyup", function (e) {
   var allowedKeys = {
@@ -142,8 +134,5 @@ document.addEventListener("keyup", function (e) {
     39: "right",
     40: "down",
   };
-
   player.handleInput(allowedKeys[e.keyCode]);
 });
-
-// If you would like you can add additional functionality to the game. You can add more code to the app.js file and to the Enemy and Player classes to accomplish that.
