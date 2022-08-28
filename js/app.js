0c1cc8dfc3fbcd68854e2f95f42ee72972029bf4
Yanyshpolska <acya2002@gmail.com>
2022-08-28 23:14:59 +0300

    Checked code for the typical mistakes

--- js/app.js
+++ js/app.js
@@ -36,26 +36,37 @@ const PLAYER_POSITION_CORRECTION = 3;
 // ===========SCORE block ====================================================
 const bestScore = document.createElement("div");
 document.body.append(bestScore);
-bestScore.style.fontSize = "2rem";
-bestScore.style.fontFamily = "Arial";
+bestScore.style.cssText = `
+font-size: 1.7rem;
+font-family: Arial;
+width: 505px;
+margin: 0px auto 20px;
+display: flex;
+justify-content: space-around;`;
 let bestLevel = 1;
 let bestStars = 0;
-bestScore.textContent =
-  "BEST SCORE: ★ - " + bestStars + ", " + "LEVEL - " + bestLevel;
 
 const score = document.createElement("div");
 document.body.append(score);
-score.style.fontSize = "2rem";
-score.style.fontFamily = "Arial";
+score.style.cssText = `
+font-size: 2rem;
+font-family: Arial;
+width: 505px;
+margin: auto;
+display: flex;
+justify-content: space-around;`;
 let level = 1;
 let stars = 0;
-score.textContent = "★ - " + stars + " _ " + "Level - " + level;
+const updateScore = function (bestStars, bestLevel, stars, level) {
+  bestScore.innerHTML = `<span>BEST SCORE: ${bestStars}</span><span>LEVEL: ${bestLevel}</span>`;
+  score.innerHTML = `<span>SCORE: ${stars}</span><span>LEVEL: ${level}</span>`;
+};
 // ===========ENEMY================================================================
-const Enemy = function (x, y, speed, victim) {
+const Enemy = function (x, y, speed, player) {
   this.speed = speed;
   this.x = x;
   this.y = y;
-  this.victim = victim;
+  this.player = player;
 
   this.rowPosition = (this.y - ENEMY_POSITION_CORRECTION) / CELL_HEIGHT + 1;
   this.sprite = "images/enemy-bug.png";
@@ -76,39 +87,32 @@ Enemy.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 Enemy.prototype.checkCollision = function () {
-  if (this.victim.y > 0) {
-    const startCollision = this.victim.x - PLAYER_WIDTH_CORRECTION;
-    const stopCollision = this.victim.x + PLAYER_WIDTH_CORRECTION;
+  if (this.player.y > 0) {
+    const startCollision = this.player.x - PLAYER_WIDTH_CORRECTION;
+    const stopCollision = this.player.x + PLAYER_WIDTH_CORRECTION;
 
     if (
-      this.rowPosition === this.victim.rowPosition &&
+      this.rowPosition === this.player.rowPosition &&
       this.x >= startCollision &&
       this.x <= stopCollision
     ) {
-      this.victim.setStartPosition();
-      allEnemies.splice(1);
+      initialisation(true);
       bestStars = stars > bestStars ? stars : bestStars;
       bestLevel = level > bestLevel ? level : bestLevel;
       stars = 0;
       level = 1;
-      score.textContent = "★ - " + stars + " _ " + "Level - " + level;
-      bestScore.textContent =
-        "BEST SCORE: ★ - " + bestStars + ", " + "LEVEL - " + bestLevel;
+      updateScore(bestStars, bestLevel, stars, level);
     }
   }
 };
-
-// =========== Player==================================================
-const Player = function (position) {
-  this.score = 0;
-  this.startPosition = position;
-  this.setStartPosition();
+// ======================Player====================================
+const Player = function () {
   this.sprite = "images/char-cat-girl.png";
 };
-Player.prototype.setStartPosition = function () {
-  this.rowPosition = this.startPosition.rowPosition;
-  this.x = this.startPosition.x;
-  this.y = this.startPosition.y - PLAYER_POSITION_CORRECTION * this.rowPosition;
+Player.prototype.setStartPosition = function (position) {
+  this.rowPosition = position.rowPosition;
+  this.x = position.x;
+  this.y = position.y - PLAYER_POSITION_CORRECTION * this.rowPosition;
   console.log(this.y);
 };
 Player.prototype.update = function (dt) {
@@ -121,18 +125,21 @@ Player.prototype.update = function (dt) {
   //  reached the water or bottom border
   if (this.y > FIELD_HEIGHT) {
     this.y = FIELD_HEIGHT - PLAYER_POSITION_CORRECTION * this.rowPosition;
-  } else if (this.y === 0) {
-    this.setStartPosition();
+  }
+  if (this.y === 0 && reachRiver === false) {
     level += 1;
-    score.textContent = "★ - " + stars + " _ " + "Level - " + level;
-    createNewEnemy(1, player);
-    star.newPosition();
+    updateScore(bestStars, bestLevel, stars, level);
+    reachRiver = true;
+    setTimeout(initialisation, 500);
   }
 };
 Player.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 Player.prototype.handleInput = function (pressedKey) {
+  if (reachRiver) {
+    return;
+  }
   switch (pressedKey) {
     case "right":
       this.x += CELL_WIDTH;
@@ -146,9 +153,10 @@ Player.prototype.handleInput = function (pressedKey) {
       break;
     case "down":
       this.y += CELL_HEIGHT - PLAYER_POSITION_CORRECTION;
-      this.rowPosition < NUMBER_OF_ROWS
-        ? (this.rowPosition += 1)
-        : this.rowPosition;
+      this.rowPosition =
+        this.rowPosition < NUMBER_OF_ROWS
+          ? (this.rowPosition += 1)
+          : this.rowPosition;
       break;
   }
 };
@@ -156,8 +164,8 @@ Player.prototype.getPosition = function () {
   return [this.x, this.rowPosition];
 };
 
-const Star = function (hunter) {
-  this.hunter = hunter;
+const Star = function (player) {
+  this.player = player;
   this.sprite = "images/Star.png";
 };
 Star.prototype.update = function (dt) {
@@ -173,22 +181,33 @@ Star.prototype.newPosition = function () {
 };
 Star.prototype.checkCollision = function () {
   if (
-    this.rowPosition === this.hunter.rowPosition &&
-    this.x === this.hunter.x
+    this.rowPosition === this.player.rowPosition &&
+    this.x === this.player.x
   ) {
     this.x = -101;
     this.y = -171;
     stars += 1;
-    score.textContent = "★ - " + stars + " _ " + "Level - " + level;
+    updateScore(bestStars, bestLevel, stars, level);
   }
 };
 
-const player = new Player(PLAYER_START_POSITION);
-
+const player = new Player();
 const star = new Star(player);
-star.newPosition();
-
 const allEnemies = [];
+let reachRiver = false;
+
+function initialisation(newGame) {
+  updateScore(bestStars, bestLevel, stars, level);
+  reachRiver = false;
+  star.newPosition();
+  player.setStartPosition(PLAYER_START_POSITION);
+  createNewEnemy(1, player);
+  if (newGame) {
+    allEnemies.splice(1);
+  }
+}
+
+initialisation();
 
 function createNewEnemy(numberOfEnemies, victim) {
   for (let i = 0; i < numberOfEnemies; i += 1) {
@@ -201,8 +220,6 @@ function createNewEnemy(numberOfEnemies, victim) {
   }
 }
 
-createNewEnemy(1, player);
-
 document.addEventListener("keyup", function (e) {
   var allowedKeys = {
     37: "left",
@@ -212,51 +229,3 @@ document.addEventListener("keyup", function (e) {
   };
   player.handleInput(allowedKeys[e.keyCode]);
 });
-
-(function addSwipeEventListener() {
-  document.addEventListener("touchstart", handleTouchStart, false); //swipes handler for move player
-  document.addEventListener("touchmove", handleTouchMove, false); //swipes handler for move player
-
-  var xDown = null;
-  var yDown = null;
-
-  function handleTouchStart(evt) {
-    const firstTouch = evt.touches[0];
-    xDown = firstTouch.clientX; //finger down
-    yDown = firstTouch.clientY; //finger down
-  }
-
-  function handleTouchMove(evt) {
-    if (!xDown || !yDown) {
-      return;
-    }
-
-    var xUp = evt.touches[0].clientX; //finger up
-    var yUp = evt.touches[0].clientY; //finger up
-
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
-
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      /*most significant*/
-      if (xDiff > 0) {
-        /* left swipe */
-        player.handleInput("left");
-      } else {
-        /* right swipe */
-        player.handleInput("right");
-      }
-    } else {
-      if (yDiff > 0) {
-        /* up swipe */
-        player.handleInput("up");
-      } else {
-        /* down swipe */
-        player.handleInput("down");
-      }
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;
-  }
-})();
